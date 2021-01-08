e0a64d0ace21271aece0df05aab71ff2e32b4715
Ross Pokorny <rpokorny@sonatype.com>
2021-01-08 11:09:39 -0500

    Further simplify example

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import { curryN, identity } from "ramda";
 
-type UnionWithNull = ((s: string) => string) | null;
+type UnionWithNull = string | null;
 
 function fnThatTakesUnionWithNull(x: UnionWithNull) {
   console.log(x);
 
-  return x ? x('foo') : 'foo';
+  return x || 'foo';
 }
 
 const curriedFn = curryN(1, fnThatTakesUnionWithNull);
 
-curriedFn(identity);
+curriedFn('bar');
 
 // this fails to typecheck even though it is a valid way to use ramda
 curriedFn(null);
